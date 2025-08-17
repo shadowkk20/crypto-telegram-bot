e97aebfce21a32c9afd06d49446d195ffd9d11e2
shadowkk20 <shuaibu4790@gmail.com>
2025-08-17 22:57:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 import requests
 import os
 
-TOKEN = os.getenv("8471294862:AAG1ixrA0GMCF4fhqwRPlDhqV7tFFXhqoS4
+TOKEN = os.getenv
 ")
-CHAT_ID = os.getenv("6300358705")
+CHAT_ID = os.getenv
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     payload = {"chat_id": CHAT_ID, "text": text}
     requests.post(url, json=payload)
-
-if __name__ == "__main__":
-    send_message("✅ Bot ya fara aiki lafiya!")
   
